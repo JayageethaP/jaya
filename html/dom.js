d4a60dc208029c506a4f9f8ca34aebb25259a9a0
jayageethaP <jayageethaskp2gmail.com>
2023-05-07 08:18:52 +0530

    adding html project

---
+++ html/dom.js
@@ -0,0 +1,12 @@
+// 1. Make the 3rd element in the list have green background color
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].textContent='Hello';
+items[2].style.backgroundColor = 'green';
+
+// 2. Make all the elements in the list have bold color font
+for (var i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = 'bold';
+}
+
