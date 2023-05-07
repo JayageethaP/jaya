694c94c34d14e3fb77b24cc2c4a8b7c0e5b5755e
jayageethaP <jayageethaskp2gmail.com>
2023-05-07 09:36:15 +0530

    update the code

--- html/dom.js
+++ html/dom.js
@@ -1,6 +1,7 @@
 // 1. Make the 3rd element in the list have green background color
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
+
 console.log(items[2]);
 items[2].textContent='Hello';
 items[2].style.backgroundColor = 'green';
@@ -13,6 +14,19 @@ for (var i = 0; i < items.length; i++) {
 var newItem = document.createElement('li');
 newItem.textContent = 'Sharp';
 document.getElementById('items').appendChild(newItem);
+
+//items[1].style.backgroundColor = 'green';
+
+//items[2].style.display = 'none';
+
+
+var secondItem = document.querySelectorAll('#items li:nth-of-type(2)');
+secondItem[0].style.color = 'green';
+var oddItems = document.querySelectorAll('#items li:nth-of-type(odd)');
+for (var i = 0; i < oddItems.length; i++) {
+  oddItems[i].style.backgroundColor = 'green';
+}
+
 
 
 
